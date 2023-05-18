a5b0a9b6b3d6219ffe715209de7368ff0c2e90e3
Marcin <marcin921292@gmail.com>
2023-05-18 21:02:57 +0200

    feat(file_handler): add new exceptions, change way to check not_permitted chars

--- file_handler.py
+++ file_handler.py
@@ -2,16 +2,16 @@ from typing import Union
 
 
 class FileHandler:
-    not_permitted = ["\"", "/", "\\", "<", ">", "?", ":", "*", "|"]
-    save_directory = "saves/"
+    NOT_PERMITTED = ('"', "/", "\\", "<", ">", "?", ":", "*", "|")
+    SAVE_DIRECTORY = "saves/"
 
     @classmethod
     def read_json_from_file(cls, file_name: str) -> Union[list[str], None]:
         result = []
-        if not cls.check_file_name_ok(file_name):
+        if not cls.__validate_file_name(file_name):
             return None
         try:
-            with open(cls.save_directory + file_name + ".txt", "r") as file:
+            with open(cls.SAVE_DIRECTORY + file_name + ".txt", "r") as file:
                 for line in file:
                     result.append(line)
         except FileNotFoundError:
@@ -20,26 +20,39 @@ class FileHandler:
         return result
 
     @classmethod
-    def write_json_to_file(cls, file_name: str, data: list[str]) -> Union[list[str], None]:
-        if not cls.check_file_name_ok(file_name):
+    def write_json_to_file(
+        cls, file_name: str, data: list[str]
+    ) -> Union[list[str], None]:
+        if not cls.__validate_file_name(file_name):
             return None
         for json in data:
-            with open(cls.save_directory + file_name + ".txt", "a") as file:
-                file.write(f'{json}\n')
+            with open(cls.SAVE_DIRECTORY + file_name + ".txt", "a") as file:
+                file.write(f"{json}\n")
+
+    @staticmethod
+    def has_forbidden_chars(file_name):
+        return any([sign for sign in file_name if sign in FileHandler.NOT_PERMITTED])
 
     @classmethod
-    def check_file_name_ok(cls, file_name: str) -> bool:
+    def __validate_file_name(cls, file_name: str) -> bool:
         try:
             if len(file_name) > 250:
-                raise ValueError
-            for sign in file_name:
-                if sign in cls.not_permitted:
-                    raise TypeError
+                raise FileNameTooLongError
+            if cls.has_forbidden_chars(file_name):
+                raise NotPermittedCharsError
             return True
-        except ValueError:
+        except FileNameTooLongError:
             print(f"File {file_name} is too long")
-        except TypeError:
-            print(f"File {file_name} contains of not permitted chars {cls.not_permitted}")
+        except NotPermittedCharsError:
+            print(
+                f"File {file_name} contains of not permitted chars {cls.NOT_PERMITTED}"
+            )
         return False
 
 
+class FileNameTooLongError(ValueError):
+    pass
+
+
+class NotPermittedCharsError(TypeError):
+    pass
